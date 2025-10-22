ddd47338b6806fdff4ec986ff7298fb4d91f70e6
amgg <2adrian2@gmail.com>
2025-10-22 14:09:58 -0700

    few more things

--- server/log.go
+++ server/log.go
@@ -1,10 +1,8 @@
 package main
 
-type LogSquasher[Entry any, Snapshot any] func(prevSnapshot *Snapshot, entries []Entry) Snapshot
+import "slices"
 
-type LogEntry[V any] struct {
-	value V
-}
+type LogSquasher[Entry any, Snapshot any] func(prevSnapshot *Snapshot, entries []Entry) Snapshot
 
 type Log[Entry any, Snapshot any] struct {
 	squasher       LogSquasher[Entry, Snapshot]
@@ -13,7 +11,7 @@ type Log[Entry any, Snapshot any] struct {
 	values         []Entry
 }
 
-func (log *Log[_Entry, _Snapshot]) SquashFirstN(n uint64) {
+func (log *Log[Entry, Snapshot]) squashFirstN(n uint64) {
 	toSquash := log.values[:n+1]
 	newValues := log.values[n+1:]
 
@@ -22,4 +20,20 @@ func (log *Log[_Entry, _Snapshot]) SquashFirstN(n uint64) {
 	log.snapshot = &newSnapshot
 	log.values = newValues
 	log.realFirstIndex += n
+}
+
+type LogEntryPredicate[Entry any] func(entry Entry) bool
+
+func (log *Log[Entry, Snapshot]) SquashUntil(predicate LogEntryPredicate[Entry]) {
+	firstNonSquashed := slices.IndexFunc(log.values, predicate)
+	// none found
+	if firstNonSquashed == -1 {
+		return
+	}
+	// none to squash (b/c first element not squash candidate)
+	if firstNonSquashed < 1 {
+		return
+	}
+
+	log.squashFirstN(uint64(firstNonSquashed - 1))
 }
