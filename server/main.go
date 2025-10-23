f49213cb541b2a5e806d427508090d31e140f41a
Caleb Fringer <cfringer98@gmail.com>
2025-10-22 18:12:00 -0700

    Implement basic ElectionServer.

--- server/main.go
+++ server/main.go
@@ -1,27 +1,73 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/djsurt/the-new-zookeepers/server/proto/raft"
+	"google.golang.org/grpc"
 )
 
+type ElectionServer struct {
+	raft.UnimplementedElectionServer
+	clientID int
+}
+
+func (s *ElectionServer) RequestVote(ctx context.Context, req *raft.VoteRequest) (*raft.Vote, error) {
+	log.Printf("Vote request received from %d", req.GetCandidateId())
+	vote := &raft.Vote{Term: 1, VoteGranted: false}
+	return vote, nil
+}
+
 func main() {
-	port := flag.Int("port", -1, "The port number to use for this node")
+	var port int
+	flag.IntVar(&port, "port", -1, "The port number to use for this node")
 	flag.Parse()
 
-	if *port < 0 {
+	// TODO: Check that the supplied port is in the cluster config.
+	if port < 0 {
 		fmt.Printf("Please provide a port number to bind to that is in the cluster file.\n")
 		os.Exit(1)
 	}
 
-	_, err := parseClusterConfig(*port, "cluster.conf")
+	_, err := parseClusterConfig(port, "cluster.conf")
 	if err != nil {
-		fmt.Printf("Error reading cluster config: %v", err)
+		fmt.Printf("Error reading cluster config: %v\n", err)
 		os.Exit(1)
 	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		fmt.Printf("Failed to create tcp listener: %v\n", err)
+		os.Exit(1)
+	}
+
+	electionServer := &ElectionServer{
+		clientID: port,
+	}
+
+	grpcServer := grpc.NewServer()
+	raft.RegisterElectionServer(grpcServer, electionServer)
+
+	serverErrChan := make(chan error)
+	go func(errChan chan<- error) {
+		err := grpcServer.Serve(listener)
+		if err != nil {
+			errChan <- err
+		}
+	}(serverErrChan)
+
+	fmt.Printf("Election server listening on port %d...\n", port)
+	// Wait for an error
+	err = <-serverErrChan
+	fmt.Printf("Error from election server: %v\n", err)
+	os.Exit(1)
 }
 
 func parseClusterConfig(myPort int, configPath string) (peers []int, err error) {
